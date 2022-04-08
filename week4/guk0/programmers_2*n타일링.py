3ab8f56c3c0968c29b8ef7779463659fa17cb864
guk0 <abcgy39@naver.com>
2022-04-08 23:49:04 +0900

    add BOJ 1043

---
+++ week4/guk0/programmers_2*n타일링.py
@@ -0,0 +1,12 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12900
+# DP
+#  % 1000000007 를 마지막에 return할때만 하면 시간초과됨. loop안에서 dp구할때 %를 계속 해줘야함.
+
+def solution(n):  
+    dp = [0] * n
+    dp[0], dp[1] = 1, 2
+    
+    for i in range(2, n):
+        dp[i] = (dp[i-1] + dp[i-2]) % 1000000007
+    
+    return dp[n-1]
